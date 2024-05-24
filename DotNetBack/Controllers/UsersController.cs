fec6424fbc5c6ac2eb86260c1be6be4f286f3880
Олександр Чалый <shurshurych2005@gmail.com>
2024-05-24 16:51:54 +0300

    Удалил юзера нахуй

--- DotNetBack/Controllers/UsersController.cs
+++ DotNetBack/Controllers/UsersController.cs
@@ -17,53 +17,5 @@ namespace DotNetBack.Controllers
         {
             _configuration = configuration;
         }
-
-        [HttpGet]
-        [Route("GetUsers")]
-
-        public JsonResult GetUsers()
-        {
-            string query = "select * from dbo.Users";
-            DataTable table = new DataTable();
-            string sqlDatasource = _configuration.GetConnectionString("ppDBCon");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
-            {
-                myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))
-                {
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                    myReader.Close();
-                    myCon.Close();
-                }
-            }
-            return new JsonResult(table);
-        }
-        
-
-        [HttpDelete]
-        [Route("DeleteUsers")]
-
-        public JsonResult DeleteUsers(int id)
-        {
-            string query = "delete from dbo.Users where id_user=@id";
-            DataTable table = new DataTable();
-            string sqlDatasource = _configuration.GetConnectionString("ppDBCon");
-            SqlDataReader myReader;
-            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
-            {
-                myCon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, myCon))
-                {
-                    myCommand.Parameters.AddWithValue("@id", id);
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                    myReader.Close();
-                    myCon.Close();
-                }
-            }
-            return new JsonResult("Deleted Successfully");
-        }
     }
 }
