73fb2e6de20f7a248711a5dca85f130003f0bb56
Алексей Игнатьев <enptydiablo28@gmail.com>
2024-05-28 00:05:49 +0300

    Поправил путь json

--- DotNetBack/Repositories/UserRepository.cs
+++ DotNetBack/Repositories/UserRepository.cs
@@ -138,7 +138,7 @@ namespace DotNetBack.Repositories
                         int userId = Convert.ToInt32(await command.ExecuteScalarAsync());
 
                         // Read JSON file content
-                        string jsonFilePath = "D:/development/DotNetBack/DotNetBack/TemplateData.json";
+                        string jsonFilePath = "C:/Users/Empty/Source/Repos/DotNetBack/DotNetBack/TemplateData.json";
                         string jsonContent = await File.ReadAllTextAsync(jsonFilePath);
 
                         // Deserialize JSON to object
