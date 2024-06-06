21de07b9971cfbb62fede1ed7b1b0230e3be38ca
Chalyy <shurshurych2005@gmail.com>
2024-06-06 20:06:20 +0300

    Update VocabDB.sql
    поменял varchar на nvarchar

--- VocabDB.sql
+++ VocabDB.sql
@@ -1,36 +1,36 @@
 CREATE TABLE Users (
     user_id INT PRIMARY KEY IDENTITY(1,1),
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    username NVARCHAR(50) NOT NULL,
+    password NVARCHAR(255) NOT NULL,
+    email NVARCHAR(100) NOT NULL,
     level INT NOT NULL,
-    subscription VARCHAR(50),
+    subscription NVARCHAR(50),
     subscription_period DATE,
-    notification_type VARCHAR(50),
-    reset_password_link VARCHAR(255),
-    role VARCHAR(50) NOT NULL,
+    notification_type NVARCHAR(50),
+    reset_password_link NVARCHAR(255),
+    role NVARCHAR(50) NOT NULL,
     notification_time TIME
 );
 
 CREATE TABLE Category (
     category_id INT PRIMARY KEY IDENTITY(1,1),
-    category_name VARCHAR(255) NOT NULL,
+    category_name NVARCHAR(255) NOT NULL,
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Word (
     word_id INT PRIMARY KEY IDENTITY(1,1),
-    name VARCHAR(100) NOT NULL,
-    translation VARCHAR(255) NOT NULL,
+    name NVARCHAR(100) NOT NULL,
+    translation NVARCHAR(255) NOT NULL,
     category_id INT,
-    img_link VARCHAR(255),
+    img_link NVARCHAR(255),
     repetition_num INT NOT NULL,
     FOREIGN KEY (category_id) REFERENCES Category(category_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Repetition (
-    reptition_id INT PRIMARY KEY IDENTITY(1,1),
+    repetition_id INT PRIMARY KEY IDENTITY(1,1),
     repetition_date DATE NOT NULL,
     word_id INT,
     FOREIGN KEY (word_id) REFERENCES Word(word_id) ON DELETE CASCADE
@@ -38,7 +38,7 @@ CREATE TABLE Repetition (
 
 CREATE TABLE Message (
     message_id INT PRIMARY KEY IDENTITY(1,1),
-    message TEXT NOT NULL,
+    message NVARCHAR(MAX) NOT NULL,
     user_id INT,
     admin_id INT,
     is_shown BIT NOT NULL,
